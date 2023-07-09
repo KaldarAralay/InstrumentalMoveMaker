287f99a6e7b083aa364cf97efd8945053af0f4e2
Kaldar <sean12111@gmail.com>
2023-07-09 11:51:08 -0400

    Update movie.py
    Added the ability to use multiple audio files at once

--- movie.py
+++ movie.py
@@ -1,11 +1,12 @@
+
 import os
 from PIL import Image
 from pydub import AudioSegment
 from moviepy.editor import *
 
 # Standard video dimensions (you can adjust these as needed)
-video_width = 1000
-video_height = 1000
+video_width = 1024
+video_height = 768
 
 # Function to check if file is an image and resize it
 def check_and_resize_image(file_path):
@@ -26,17 +27,25 @@ def is_audio(file_path):
     except:
         return False
 
-def create_video(image_file, audio_file, root):
+def create_video(image_file, audio_files, root):
     print("Creating video...")
-    audio = AudioSegment.from_file(audio_file)
-    duration = len(audio) / 1000  # Duration of audio file in seconds
+    # Concatenate all audio files into one
+    combined_audio = AudioSegment.empty()
+    for audio_file in audio_files:
+        combined_audio += AudioSegment.from_file(audio_file)
+
+    duration = len(combined_audio) / 1000  # Duration of audio file in seconds
+
+    # Save combined audio to a file
+    combined_audio_path = os.path.join(root, "combined_audio.wav")
+    combined_audio.export(combined_audio_path, format="wav")
 
     # Use the image file to create a video clip
     clip = ImageSequenceClip([image_file], durations=[duration])
     clip.fps = 24  # Set the frames per second
 
     # Create the final video clip with audio
-    final_clip = clip.set_audio(AudioFileClip(audio_file))
+    final_clip = clip.set_audio(AudioFileClip(combined_audio_path))
 
     # Write the result to a file
     final_clip.write_videofile(os.path.join(root, "video.mp4"), fps=24)
@@ -52,9 +61,8 @@ def main():
             print(f"Checking directory: {root}")
             # Check if directory has both image and audio
             has_image = False
-            has_audio = False
             image_file = ""
-            audio_file = ""
+            audio_files = []
             for file in os.listdir(root):
                 file_path = os.path.join(root, file)
                 if check_and_resize_image(file_path):
@@ -62,15 +70,13 @@ def main():
                     image_file = file_path
                     print(f"Image file found: {file_path}")
                 if is_audio(file_path):
-                    has_audio = True
-                    audio_file = file_path
+                    audio_files.append(file_path)
                     print(f"Audio file found: {file_path}")
 
-                # If both image and audio files are found, create video
-                if has_image and has_audio:
-                    # Create the video
-                    create_video(image_file, audio_file, root)
-                    break
+            # If both image and audio files are found, create video
+            if has_image and audio_files:
+                # Create the video
+                create_video(image_file, audio_files, root)
 
 if __name__ == '__main__':
     main()
